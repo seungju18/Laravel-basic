e559a72e5e7fad90d5f80b30fa762e955f02294b
seungju18 <43843214+seungju18@users.noreply.github.com>
2024-02-21 14:57:25 +0900

    Create a Simple writing page with csrf function

---
+++ laravel-basic/resources/views/articles/create.blade.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <meta charset = "utf-8">
+        <meta name = "viewport" content = "width=device-width , inital-scale=1.0">
+        @vite(['resources/css/app.css', 'resources/css/app.js'])
+    </head>
+    <body>
+        <div class = "container p-5">
+            <h1 class = "text-2xl">글쓰기
+            <form action="/articles" method = "POST" class="mt-3">
+                @csrf
+                <input type = "text" class = "black w-full mb-2 rounded">
+                <button class = "px-3 py-1 bg-black text-white rounded text-xs">저장하기</button>
+            </form>
+        </div>
+    </body>
+</html>
